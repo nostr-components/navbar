6df82ffe5f241febcac05bd129546869576ba02c
Melvin Carvalho <melvincarvalho@gmail.com>
2023-05-21 15:06:04 +0200

    move to lib

--- app.js
+++
@@ -1,4 +0,0 @@
-import { html, render } from './js/standalone.module.js'
-import Navbar from './index.js'
-
-render(html`<${Navbar} />`, document.body)
---
+++ js/app.js
@@ -0,0 +1,4 @@
+import { html, render } from '../js/standalone.module.js'
+import Navbar from '../lib/index.js'
+
+render(html`<${Navbar} />`, document.body)
